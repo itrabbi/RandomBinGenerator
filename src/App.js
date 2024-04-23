53a64e4f30d62ea2fc4831e9001e9fcf527c6569
Rabbi Sudo <rabbisudo@gmail.com>
2024-04-24 00:33:16 +0600

    Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import Footer from './Footer';
 import RandomBinGenerator from './RandomBinGenerator';
 
 function App() {
   console.log("Rabbi Sudo (itrabbi.com)")
+  const footerStyle = {
+    backgroundColor: '#fff',
+    padding: '10px',
+    textAlign: 'center',
+    position: 'fixed',
+    left: '0',
+    bottom: '0',
+    width: '100%',
+  };
+
   return (
     <>
     <RandomBinGenerator />
+    <div style={footerStyle}>
+    <Footer />
+    </div>
     </>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return (
+        <div className="container">
+          <p className='copyright'> <a href="https://me.itrabbi.com/" target='_blank'>Rabbi Hassan</a>. All rights reserved</p>
+        </div>
+    );
+};
+
+export default Footer;
